b21923f16b427a9ff64281c0b60f0fa902bdbd64
Rowan Miller <mail@romiller.com>
2015-10-22 15:25:14 -0700

    :memo: doc comments for Infrastructure.*

--- src/EntityFramework.Core/Infrastructure/CoreLoggingEventId.cs
+++ src/EntityFramework.Core/Infrastructure/CoreLoggingEventId.cs
@@ -1,15 +1,43 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.Extensions.Logging;
+
 namespace Microsoft.Data.Entity.Infrastructure
 {
+    /// <summary>
+    ///     Values that are used as the eventId when logging messages from this assembly via <see cref="ILogger"/>.
+    /// </summary>
     public enum CoreLoggingEventId
     {
+        /// <summary>
+        ///     An error occurred while accessing the database.
+        /// </summary>
         DatabaseError = 1,
+
+        /// <summary>
+        ///     A LINQ query is being translated in SQL.
+        /// </summary>
         CompilingQueryModel,
+
+        /// <summary>
+        ///     The object model from a LINQ query is being optimized.
+        /// </summary>
         OptimizedQueryModel,
+
+        /// <summary>
+        ///     The related data that was included in a LINQ query is being processed.
+        /// </summary>
         IncludingNavigation,
+
+        /// <summary>
+        ///     Results from a LINQ query that should be tracked by the context are being processed.
+        /// </summary>
         TrackingQuerySources,
+
+        /// <summary>
+        ///     The query plan for a LINQ query is being calculated.
+        /// </summary>
         QueryPlan
     }
 }
--- src/EntityFramework.Core/Infrastructure/ModelSource.cs
+++ src/EntityFramework.Core/Infrastructure/ModelSource.cs
@@ -11,6 +11,16 @@ using Microsoft.Data.Entity.Utilities;
 
 namespace Microsoft.Data.Entity.Infrastructure
 {
+    /// <summary>
+    ///     <para>
+    ///         A base implementation of <see cref="IModelSource"/> that produces a model based on the <see cref="DbSet{TEntity}"/> properties
+    ///         exposed on the context. The model is cached to avoid recreating it every time it is requested.
+    ///     </para>
+    ///     <para>
+    ///         This type is typically used by database providers (and other extensions). It is generally
+    ///         not used in application code.
+    ///     </para>
+    /// </summary>
     public abstract class ModelSource : IModelSource
     {
         private readonly ThreadSafeDictionaryCache<Type, IModel> _models = new ThreadSafeDictionaryCache<Type, IModel>();
@@ -28,9 +38,23 @@ namespace Microsoft.Data.Entity.Infrastructure
             CoreConventionSetBuilder = coreConventionSetBuilder;
         }
 
+        /// <summary>
+        ///     Returns the model from the cache, or creates a model if it is not present in the cache.
+        /// </summary>
+        /// <param name="context"> The context the model is being produced for. </param>
+        /// <param name="conventionSetBuilder"> The convention set to use when creating the model. </param>
+        /// <param name="validator"> The validator to verify the model can be successfully used with the context. </param>
+        /// <returns> The model to be used. </returns>
         public virtual IModel GetModel(DbContext context, IConventionSetBuilder conventionSetBuilder, IModelValidator validator)
             => _models.GetOrAdd(context.GetType(), k => CreateModel(context, conventionSetBuilder, validator));
 
+        /// <summary>
+        ///     Creates the model. This method is called when the model was not found in the cache.
+        /// </summary>
+        /// <param name="context"> The context the model is being produced for. </param>
+        /// <param name="conventionSetBuilder"> The convention set to use when creating the model. </param>
+        /// <param name="validator"> The validator to verify the model can be successfully used with the context. </param>
+        /// <returns> The model to be used. </returns>
         protected virtual IModel CreateModel(
             [NotNull] DbContext context,
             [CanBeNull] IConventionSetBuilder conventionSetBuilder,
@@ -57,6 +81,12 @@ namespace Microsoft.Data.Entity.Infrastructure
             return model;
         }
 
+        /// <summary>
+        ///     Creates the convention set to be used for the model. Uses the <see cref="CoreConventionSetBuilder"/>
+        ///     if <paramref name="conventionSetBuilder"/> is null.
+        /// </summary>
+        /// <param name="conventionSetBuilder"> The convention set builder to be used. </param>
+        /// <returns> The convention set to be used. </returns>
         protected virtual ConventionSet CreateConventionSet([CanBeNull] IConventionSetBuilder conventionSetBuilder)
         {
             var conventionSet = CoreConventionSetBuilder.CreateConventionSet();
@@ -65,6 +95,11 @@ namespace Microsoft.Data.Entity.Infrastructure
                 : conventionSetBuilder.AddConventions(conventionSet);
         }
 
+        /// <summary>
+        ///     Adds the entity types found in <see cref="DbSet{TEntity}"/> properties on the context to the model.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        /// <param name="context"></param>
         protected virtual void FindSets([NotNull] ModelBuilder modelBuilder, [NotNull] DbContext context)
         {
             foreach (var setInfo in SetFinder.FindSets(context))
@@ -73,6 +108,11 @@ namespace Microsoft.Data.Entity.Infrastructure
             }
         }
 
+        /// <summary>
+        ///     Runs <see cref="DbContext.OnModelCreating(ModelBuilder)"/> from the context.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="modelBuilder"></param>
         public static void OnModelCreating([NotNull] DbContext context, [NotNull] ModelBuilder modelBuilder)
             => context.OnModelCreating(modelBuilder);
     }
